589836951090ccc4e2beb49ddcff6a78695507c3
Jan Böker <naj_b@gmx.de>
2021-05-17 22:08:12 +0200

    add first number buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,26 +4,32 @@ function App() {
 
   //const state = React.useState();
   // Example: Destructuring in ES6
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState("");
 
   const safariRenderHack = { opacity: count % 2 ? 1 : 0.99 };
 
-  //const [red, green, blue] = [64, 115, 158];
-  //console.log(red);
-
-  function increase() {
-    setCount(count + 1);
-  }
-
-  function decrease() {
-    setCount(count - 1);
+  function plusOne() {
+    setCount(count + "1");
   }
 
   return (
     <div className="container">
       <h1 style={safariRenderHack}>{count}</h1>
-      <button onClick={decrease} >-</button>
-      <button onClick={increase} >+</button>
+      <div>
+        <button onClick={plusOne}>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </div>
+      <div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
     </div>
   );
 }
